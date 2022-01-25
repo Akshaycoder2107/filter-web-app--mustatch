83dd48297ddebafaeafd573704177080308f317b
Akshaycoder2107 <87129350+Akshaycoder2107@users.noreply.github.com>
2022-01-25 16:34:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 function preload(){}
-function draw(){}
+function draw(){
+    image(video,0,0,300,300);
+}
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotResults);
 }
 function take_snapshot(){
     save("myfilter.png");
+}
+function modelLoaded(){
+    console.log("poseNet is Initialized");
+}
+function gotResults(results){
+if(results.length>0){
+    console.log(results);
+    console.log("noseX="+results[0].pose.nose.x);
+    console.log("noseY="+results[0].pose.nose.y);
+}
 }
